ac6264176f6e850ad9598f7e8c6291d6ad236d2f
taharu-bless77 <s1f102401742@iniad.org>
2025-06-26 13:44:40 +0900

    Taskモデルを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
+from django.utils import timezone
+from datetime import datetime
+from todo.models import Task
 
-# Create your tests here.
-class SampleTestCase(TestCase):
-    def test_sample1(self):
-        self.assertEqual(1 + 2, 3)
+class TaskModelTestCase(TestCase):
+    def test_create_task1(self):
+        # テスト用の締切日時データを作成
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
 
-        
+        # データベースにタスクを作成して保存
+        task = Task(title='task1', due_at=due)
+        task.save()
 
+        # データベースから今保存したタスクを再度取得
+        task = Task.objects.get(pk=task.pk)
+
+        # 取得したタスクの各フィールドが期待通りの値かチェック
+        self.assertEqual(task.title, 'task1')
+        self.assertFalse(task.completed)
+        self.assertEqual(task.due_at, due)
